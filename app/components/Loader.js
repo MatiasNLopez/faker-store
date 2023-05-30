4a9366966bb432ad4389b19d967504ef4a183848
MatiasNLopez <lopezmatias36@gmail.com>
2023-05-30 18:07:59 -0300

    feat: loader component

--- app/components/Loader.js
+++ app/components/Loader.js
@@ -1,3 +1,9 @@
+import conf from '../config/const.js'
+
 export function Loader(){
-    
+    const $loader = document.createElement('img');
+    $loader.src = conf.icons.spiner;
+    $loader.alt= "Cargando...";
+    $loader.classList.add('Loader');
+    return $loader;
 }
