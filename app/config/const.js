19f912a2dec0fdb5b0ce320aaf2a0ce502f1e1fb
MatiasNLopez <lopezmatias36@gmail.com>
2023-05-30 18:07:00 -0300

    const routes and icons

---
+++ app/config/const.js
@@ -0,0 +1,21 @@
+
+export default {
+    rutes:{
+        home: "#/",
+        favorite: "#/favorite",
+        cart: "#/cart",
+        profile: "#/profile",
+        search:"#/search"
+    },
+    icons:{
+        cart: "/app/assets/img/cart.svg",
+        cartSeclect: "/app/assets/img/cart-select.svg",
+        favorite: "/app/assets/img/favorite.svg",
+        favoriteSelect: "/app/assets/img/favorite-select.svg",
+        home: "/app/assets/img/home.svg",
+        homeSelect: "/app/assets/img/home-select.svg",
+        profile: "/app/assets/img/profile.svg",
+        profileSelect: "/app/assets/img/profile-select.svg",
+        spiner: "app/assets/img/spinning-circles.svg"
+    }
+}
