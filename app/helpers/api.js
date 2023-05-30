c4969f5a491cfc9e6b0bf7d9b6d3365433087c30
MatiasNLopez <lopezmatias36@gmail.com>
2023-05-30 18:10:08 -0300

    fix: api faker store

--- app/helpers/api.js
+++ app/helpers/api.js
@@ -1,22 +1,25 @@
 const OFFSET=0,
     LIMIT=10,
+    API = 'https://fakestoreapi.com',
+    CARTS = `${API}/carts`,
     /* Return products from OFFSET (0) to LIMIT (10)*/
-    PRODUCTS = `https://api.escuelajs.co/api/v1/products?offset=${OFFSET}&limit=${LIMIT}`, 
-    USERS = 'https://api.escuelajs.co/api/v1/users',
-    CHECK_EMAIL='https://api.escuelajs.co/api/v1/users/is-available',
-    CATEGORIES='https://api.escuelajs.co/api/v1/categories',
-    AUTH = 'https://api.escuelajs.co/api/v1/auth/login',
-    UPLOAD_FILES='https://api.escuelajs.co/api/v1/files/upload',
+    PRODUCTS = `${API}/products`, 
+    USERS = `${API}/users`,
+    /* CHECK_EMAIL='https://api.escuelajs.co/api/v1/users/is-available', */
+    CATEGORIES=`${API}/products/categories`,
+    AUTH = `${API}/auth/login`
+    /* UPLOAD_FILES='https://api.escuelajs.co/api/v1/files/upload', */
     /* https://api.escuelajs.co/api/v1/files/{file} */
-    GET_FILE='https://api.escuelajs.co/api/v1/files/';
+    /* GET_FILE='https://api.escuelajs.co/api/v1/files/'; */
 
 export default {
     OFFSET,
+    CARTS,
     PRODUCTS,
     USERS,
-    CHECK_EMAIL,
+    /* CHECK_EMAIL, */
     CATEGORIES,
     AUTH,
-    UPLOAD_FILES,
-    GET_FILE,
+    /* UPLOAD_FILES, */
+    /* GET_FILE, */
 }
