eb668f90fb4d5c2671cf56e997083307a6bc5738
MatiasNLopez <lopezmatias36@gmail.com>
2023-05-31 22:55:29 -0300

    style: product card

--- app/components/ProductCard.js
+++ app/components/ProductCard.js
@@ -1,20 +1,16 @@
 export function ProductCard(props) {
    const {category, description, image, price, title} = props;
+   
     return ` 
         <article class="product-card">
-            <img src="${image}" alt="${title}">
-            <h1>${title}</h1>
-            <p>${price}</p>
-            <span><button>+</button></span>
+            <div class="image-content">
+                <img src="${image}" alt="${title}">
+            </div>
+            <div class="product-card_description">
+                <p>${title}</p>
+                <h3>$${price}</h3>
+                <span><button>+</button></span>
+            </div>
         </article>
     `
-    
-    /* 
-        title: 'test product',
-        price: 13.5,
-        description: 'lorem ipsum set',
-        image: 'https://i.pravatar.cc',
-        category: 'electronic'
-    
-    */
 }
