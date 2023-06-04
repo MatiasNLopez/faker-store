731294e365c46e81f397dc0e53a0f4f3e68098c7
MatiasNLopez <lopezmatias36@gmail.com>
2023-06-04 01:03:10 -0300

    style: product card

--- app/components/ProductCard.js
+++ app/components/ProductCard.js
@@ -1,10 +1,13 @@
 export function ProductCard(props) {
-   const {category, description, id, image, price, title} = props;
+   const {category, description, id, image, price, title, rating} = props;
    
     return ` 
         <article class="product-card" data-id="${id}">
             <div class="image-content">
                 <img src="${image}" alt="${title}">
+                <span>  
+                    <small>&#9733; ${rating.rate} ${rating.count} Opiniones</small>
+                </span>
             </div>
             <div class="product-card_description">
                 <p>${title}</p>
