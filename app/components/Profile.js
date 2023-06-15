a082ce547578660ec8dee6d2320e4224a645b583
MatiasNLopez <lopezmatias36@gmail.com>
2023-06-15 15:19:33 -0300

    feat: add title yo each section profile

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -15,17 +15,20 @@ export const Profile = new Component({
                     <article class="profile" data-id="${user.id}">
                         <section class="profile_acount">
                             <h3><a href="">Editar</a></h3>
+                            <h2>Datos de la cuenta</h2>
                             <p><strong>Usuario:</strong> ${user.username}</p>
                             <p><strong>Email:</strong> ${user.email}</p>
                         </section>
                         <section class="profile_person" >
                             <h3><a href="">Editar</a></h3>
+                            <h2>Datos Personales</h2>
                             <p><strong>Nombre:</strong> ${user.name.firstname}</p>
                             <p><strong>Apellido:</strong> ${user.name.lastname}</p>
                             <p><strong>Telefono:</strong> ${user.phone}</p>
                         </section>
                         <section class="profile_address" >
                             <h3><a href="">Editar</a></h3>
+                            <h2>Direccion</h2>
                             <p><strong>Ciudad:</strong> ${user.address.city}</p>
                             <p><strong>Codigo: postal</strong> ${user.address.zipcode}</p>
                             <p><strong>Dreccion:</strong> ${user.address.street} - ${user.address.number} </p>
